a2aa896d7f3266a986105ffdacab12706741e562
artur_hurynovich <artur.hurynovich@gmail.com>
2022-02-28 22:13:15 +0300

    Initial commit

---
+++ src/main/java/com/hurynovich/data_unit_service/DataUnitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hurynovich.data_unit_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataUnitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataUnitServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hurynovich/data_unit_service/DataUnitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hurynovich.data_unit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataUnitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
